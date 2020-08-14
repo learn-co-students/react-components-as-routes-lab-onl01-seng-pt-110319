7ab5424b8486a020de285a3b70a83a39a0053674
Chrispolanco <chrispolanco@sbcglobal.net>
2020-08-13 22:40:13 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => (
+          <div>
+              <p>Name: {director.name}</p>
+              <p>Movies:</p>
+              <ul>
+                  {director.movies.map(movie => (
+                      <li>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 }
