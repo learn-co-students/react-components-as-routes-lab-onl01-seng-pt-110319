c590ba1f5ba7fb0e19061a213617d5290f941684
Mug <brad.alexander913@gmail.com>
2020-08-22 13:13:58 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,20 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor => (
+        <div>
+          <p>Actor: {actor.name}</p>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie => (
+              <p>{movie}</p>
+            )))}
+          </ul>
+        </div>
+      )))}
     </div>
-  );
+  )
 };
 
 export default Actors;
