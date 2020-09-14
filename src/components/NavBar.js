388ebc12c00ebb9f3f5a53556ebd96117940a81a
github username <github email address>
2020-09-13 21:46:38 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '80px',
+  padding: '1px',
+  margin: '0px 3px 3px',
+  background: 'darkblue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink to="/" exact style={link} activeStyle={{background: "navy"}}>Home</NavLink>
+      <NavLink to="/movies" exact style={link} activeStyle={{background: "navy"}}>Movies</NavLink>
+      <NavLink to="/directors" exact style={link} activeStyle={{background: "navy"}}>Directors</NavLink>
+      <NavLink to="/actors" exact style={link} activeStyle={{background: "navy"}}>Actors</NavLink>
     </div>
   );
 };
